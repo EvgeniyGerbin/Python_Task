8a484a95a3678395b2f2f7064c3d86f26ce167bb
Evgeniy Gerbin <zenagerbin@gmail.com>
2022-09-20 19:09:30 +0300

    work on mistakes

--- Game /fight.py
+++ Game /fight.py
@@ -1,7 +1,6 @@
-import sys
-from exceptions import InputError
 from models import *
 import random
+
 
 print(
     '\nWARRIOR [1]------>Stronger than a wizard but, weaker than a rider!'
@@ -13,39 +12,35 @@ print(
 )
 char1 = int(input())
 
-if char1 == 1:
-    char1 = warrior
-elif char1 == 2:
-    char1 = wizard
-elif char1 == 3:
-    char1 = archer
-elif char1 == 4:
-    char1 = rider
-else:
-    raise InputError
+match char1:
+    case 1:
+        char1 = warrior
+    case 2:
+        char1 = wizard
+    case 3:
+        char1 = archer
+    case 4:
+        char1 = wizard
+    case _:
+        print('Input incorrect!')
+
 
 char2 = characters[random.randint(0, 3)]
-print(f'{char1.name} and {char2.name} will fight!!!')
 
 
 def menu():
+    print(f'{char1.name} and {char2.name} will fight!!!')
     while True:
         print('1) Let`s Fight!')
         print('2) View info!')
-        try:
-            n = int(input())
-
-            if n == 1:
-                fight()
-            if n == 2:
+        n = input()
+        match n:
+            case '1':
+                char1.fight(char2)
+            case '2':
                 menu_stats()
-
-
-
-        except NameError:
-            print('Input int!')
-        except SyntaxError:
-            print('Input int!')
+            case _:
+                print('Incorrect input, please try again!')
 
 
 def menu_stats():
@@ -55,49 +50,6 @@ def menu_stats():
     print("Enemy: ")
     char2.character_info()
     input('Press Enter to continue!')
-
-
-def fight():
-    if char1 == warrior and char2 == wizard:
-        char1.damage = char1.damage + (char1.damage * 0.15)
-    elif char1 == wizard and char2 == archer:
-        char1.damage = char1.damage + (char1.damage * 0.15)
-    elif char1 == archer and char2 == rider:
-        char1.damage = char1.damage + (char1.damage * 0.15)
-    elif char1 == rider and char2 == warrior:
-        char1.damage = char1.damage + (char1.damage * 0.15)
-    elif char2 == warrior and char1 == wizard:
-        char2.damage = char2.damage + (char2.damage * 0.15)
-    elif char2 == wizard and char1 == archer:
-        char2.damage = char2.damage + (char2.damage * 0.15)
-    elif char2 == archer and char1 == rider:
-        char2.damage = char2.damage + (char2.damage * 0.15)
-    elif char2 == rider and char1 == warrior:
-        char2.damage = char2.damage + (char2.damage * 0.15)
-    while char1.heath or char2.heath <= 0:
-        print(f'{char1.name}: {int(char1.heath)}hp, {char1.damage}dmg')
-        print(f'{char2.name}: {int(char2.heath)}hp, {char2.damage}dmg')
-        print("**********************")
-        print('Press 1 to attack the enemy!')
-        n = int(input())
-        if n == 1:
-            luck = random.randint(0, 100)
-            if luck <= char1.luck:
-                char2.heath -= char1.damage + (char1.damage * char1.critical)
-            else:
-                char2.heath -= char1.damage
-            luck = random.randint(0, 100)
-            if luck <= char2.luck:
-                char1.heath -= char2.damage + (char2.damage * char2.critical)
-            else:
-                char1.heath -= char2.damage
-        if char1.heath <= 0:
-            print('You lose!')
-            sys.exit()
-        if char2.heath <= 0:
-            print('You win! ')
-            sys.exit()
-
 
 if __name__ == '__main__':
     menu()
--- Game /models.py
+++ Game /models.py
@@ -1,4 +1,7 @@
+import random
+import sys
 from enum import Enum
+
 
 class CharacterType(Enum):
     WARRIOR = 0,
@@ -10,41 +13,100 @@ class CharacterType(Enum):
 class Character:
     def __init__(self, name, health, damage, critical, luck):
         self.name = name
-        self.heath = health
+        self.health = health
         self.damage = damage
         self.critical = critical
         self.luck = luck
 
     def character_info(self):
-        return print(f'Character: {self.name} \n'
-                     f'Health: {self.heath}\n'
+        return print(f'Character: {self.name}\n'
+                     f'Health: {self.health}\n'
                      f'Damage: {self.damage}\n'
                      f'Critical damage: {round(self.critical * 100)}%\n'
                      f'Luck: {self.luck}%')
 
+    def dmg(self):
+        lucky = random.randint(1, 100)
+        if lucky <= self.luck:
+            crit = self.damage + (self.damage * self.critical)
+            return crit
+        else:
+            return self.damage
+
+    def fight(self, enemy_obj):
+        move = 1
+        while self.health or enemy_obj.health <= 0:
+            print("**********************")
+            print(f'MOVE {move}')
+            print(f'{self.name}: {int(self.health)}hp, {self.damage}dmg')
+            print(f'{enemy_obj.name}: {int(enemy_obj.health)}hp, {enemy_obj.damage}dmg')
+
+
+            enemy_obj.health -= self.dmg()
+            self.health -= enemy_obj.dmg()
+            move = move + 1
+            if self.health <= 0:
+                print('You lose!')
+                sys.exit()
+            if enemy_obj.health <= 0:
+                print('You win! ')
+                sys.exit()
+
+
 class Warrior(Character):
 
     def __init__(self):
-        super().__init__('Warrior', 150, 17, 0.15, 15)
+        super().__init__('Warrior', 150, 10, 0.15, 30)
+
+    def fight(self, enemy_obj):
+        if isinstance(enemy_obj, Wizard):
+            self.damage = self.damage + (self.damage * 0.15)
+        else:
+            self.damage = self.damage
+        super().fight(enemy_obj)
+
 
 class Wizard(Character):
 
     def __init__(self):
-        super().__init__('Wizard', 80, 20, 0.3, 10)
+        super().__init__('Wizard', 80, 20, 0.3, 30)
+
+    def fight(self, enemy_obj):
+        if isinstance(enemy_obj, Archer):
+            self.damage = self.damage + (self.damage * 0.15)
+        else:
+            self.damage = self.damage
+        super().fight(enemy_obj)
+
 
 class Archer(Character):
 
     def __init__(self):
-        super().__init__('Archer', 100, 15, 0.3, 20)
+        super().__init__('Archer', 100, 15, 0.3, 100)
+
+    def fight(self, enemy_obj):
+        if isinstance(enemy_obj, Rider):
+            self.damage = self.damage + (self.damage * 0.15)
+        else:
+            self.damage = self.damage
+
+        super().fight(enemy_obj)
+
 
 class Rider(Character):
 
     def __init__(self):
         super().__init__('Rider', 160, 18, 0.2, 18)
 
+    def fight(self, enemy_obj):
+        if isinstance(enemy_obj, Warrior):
+            self.damage = self.damage + (self.damage * 0.15)
+        else:
+            self.damage = self.damage
+        super().fight(enemy_obj)
+
 
 def char(char_type: CharacterType):
-
     char_dict = {
         CharacterType.WARRIOR: Warrior,
         CharacterType.WIZARD: Wizard,
@@ -53,11 +115,10 @@ def char(char_type: CharacterType):
     }
     return char_dict[char_type]()
 
+
 warrior = char(CharacterType.WARRIOR)
 wizard = char(CharacterType.WIZARD)
 archer = char(CharacterType.ARCHER)
 rider = char(CharacterType.RIDER)
 
 characters = [warrior, wizard, archer, rider]
-
-
